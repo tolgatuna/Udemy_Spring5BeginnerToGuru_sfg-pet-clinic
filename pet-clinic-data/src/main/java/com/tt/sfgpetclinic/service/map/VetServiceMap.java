87b5e610ca918eb95e4145f0a32c9675dad0c28f
tolgatuna <tolga.tuna@etstur.com>
2018-07-27 13:49:46 +0300

    implement map based services.

---
+++ pet-clinic-data/src/main/java/com/tt/sfgpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.tt.sfgpetclinic.service.map;
+
+import com.tt.sfgpetclinic.model.Vet;
+import com.tt.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
