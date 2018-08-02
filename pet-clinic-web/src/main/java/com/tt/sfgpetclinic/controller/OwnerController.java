a7e86002c5ba8b9858b5ceb31ea542411f24803f
tolgatuna <tolga.tuna@etstur.com>
2018-08-02 14:12:03 +0300

    89. Spring Pet Clinic - List Owners.

--- pet-clinic-web/src/main/java/com/tt/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/tt/sfgpetclinic/controller/OwnerController.java
@@ -1,13 +1,27 @@
 package com.tt.sfgpetclinic.controller;
 
+import com.tt.sfgpetclinic.model.Owner;
+import com.tt.sfgpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        Set<Owner> owners = ownerService.findAll();
+        model.addAttribute("owners", owners);
         return "owners/index";
     }
 }
