bb68eedf6e14ce76d47a35269afa03be69f1623e
tolgatuna <tolga.tuna@etstur.com>
2018-08-02 14:19:56 +0300

    90. Spring Pet Clinic - List Vets.

--- pet-clinic-web/src/main/java/com/tt/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/tt/sfgpetclinic/controller/VetController.java
@@ -1,13 +1,26 @@
 package com.tt.sfgpetclinic.controller;
 
+import com.tt.sfgpetclinic.model.Vet;
+import com.tt.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        Set<Vet> vets = vetService.findAll();
+        model.addAttribute("vets", vets);
         return "vets/index";
     }
 }
