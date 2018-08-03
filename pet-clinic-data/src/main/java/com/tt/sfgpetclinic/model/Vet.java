211fe17c890d38219830d4fe088e8ebd8edb7f46
tolgatuna <tolga.tuna@etstur.com>
2018-08-03 15:56:41 +0300

    Section 7 - Spring Pet Clinic - Create Vet Speciality Entity, Add to Vet

--- pet-clinic-data/src/main/java/com/tt/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/tt/sfgpetclinic/model/Vet.java
@@ -1,7 +1,16 @@
 package com.tt.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
 
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
