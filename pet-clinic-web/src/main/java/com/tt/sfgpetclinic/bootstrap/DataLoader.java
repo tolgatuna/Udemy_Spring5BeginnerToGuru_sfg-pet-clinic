e9022219535945fb3b63041bce0820349377be9d
tolgatuna <tolga.tuna@etstur.com>
2018-08-03 17:34:25 +0300

    Section 7 - Spring Pet Clinic - Enhance Owners with Pets and Contact Info

--- pet-clinic-web/src/main/java/com/tt/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/tt/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.tt.sfgpetclinic.bootstrap;
 
 import com.tt.sfgpetclinic.model.Owner;
+import com.tt.sfgpetclinic.model.Pet;
 import com.tt.sfgpetclinic.model.PetType;
 import com.tt.sfgpetclinic.model.Vet;
 import com.tt.sfgpetclinic.service.OwnerService;
@@ -8,6 +9,8 @@ import com.tt.sfgpetclinic.service.PetTypeService;
 import com.tt.sfgpetclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -35,12 +38,33 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("12321321321312");
+
+        Pet michealPet = new Pet();
+        michealPet.setName("ZuuZuu");
+        michealPet.setPetType(dogPetType);
+        michealPet.setBirthDate(LocalDate.now());
+        michealPet.setOwner(owner1);
+        owner1.getPets().add(michealPet);
 
         ownerService.save(owner1);
+
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Gleanne");
+        owner2.setAddress("333 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("4556565666555");
+
+        Pet fionaPet = new Pet();
+        fionaPet.setName("Caty");
+        fionaPet.setPetType(catPetType);
+        fionaPet.setBirthDate(LocalDate.now());
+        fionaPet.setOwner(owner1);
+        owner2.getPets().add(fionaPet);
 
         ownerService.save(owner2);
 
